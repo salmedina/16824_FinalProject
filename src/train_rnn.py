923ac036995e455f1f5f69f9c5b224925c803bd3
salmedina <salbed.medina@gmail.com>
2017-05-01 03:20:47 -0400

    add gpu id and test/train mode

--- src/train_rnn.py
+++ src/train_rnn.py
@@ -14,7 +14,7 @@ import numpy as np
 RNNParams = namedtuple('RNNParams', 'sequence_length, input_size, hidden_size, num_layers, num_classes, batch_size, num_epochs, learning_rate')
 
 use_cuda = torch.cuda.is_available()
-
+gpu_id = 0
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
@@ -31,11 +31,9 @@ class RNN(nn.Module):
         h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
         c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
         if use_cuda:
-            h0 = h0.cuda()
-            c0 = c0.cuda()
+            h0 = h0.cuda(gpu_id)
+            c0 = c0.cuda(gpu_id)
         out, _ = self.lstm(x, (h0, c0))
-        #if use_cuda:
-        #    out = out.cuda()
         out = self.fc(out[:, -1, :])
 
         return out
@@ -77,20 +75,20 @@ def train(train_dataset, rnn_params, save_path=''):
     optimizer = torch.optim.Adam(rnn.parameters(), lr=rnn_params.learning_rate)
 
     if use_cuda:
-        rnn = rnn.cuda()
-        criterion = criterion.cuda()
+        rnn = rnn.cuda(gpu_id)
+        criterion = criterion.cuda(gpu_id)
 
     for epoch in range(rnn_params.num_epochs):
         for i, (images, labels) in enumerate(train_loader):
             images = Variable(images.view(-1, rnn_params.sequence_length, rnn_params.input_size))
             if use_cuda:
-                images = images.cuda()
+                images = images.cuda(gpu_id)
             optimizer.zero_grad()
             outputs = rnn(images)
             labels = Variable(labels)
             if use_cuda:
-                outputs = outputs.cuda()
-                labels = labels.cuda()
+                outputs = outputs.cuda(gpu_id)
+                labels = labels.cuda(gpu_id)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -114,24 +112,31 @@ def eval(rnn, test_dataset, rnn_params):
     for images, labels in test_loader:
         images = Variable(images.view(-1, rnn_params.sequence_length, rnn_params.input_size))
         if use_cuda:
-            images = images.cuda()
+            images = images.cuda(gpu_id)
         outputs = rnn(images)
 
         if use_cuda:
-            labels = labels.cuda()
-            outputs = outputs.cuda()
+            labels = labels.cuda(gpu_id)
+            outputs = outputs.cuda(gpu_id)
         _, predicted = torch.max(outputs.data, 1)
 
         total += labels.size(0)
         correct += (predicted.view(-1) == labels.view(-1)).sum()
     print 'Test Accuracy of the model on the %d test images: %d%%' % (len(test_dataset),100 * correct / total)
 
+def test(state_dict_path, test_dataset, rnn_params):
+    rnn = RNN(rnn_params.input_size, rnn_params.hidden_size, rnn_params.num_layers, rnn_params.num_classes)
+    rnn.load_state_dict(torch.load(state_dict_path))
+    eval(rnn, test_dataset, rnn_params)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print 'Usage: train_rnn.py <config_path>'
         sys.exit(-1)
     config = json.load(open(sys.argv[1]))
+
+    if use_cuda and 'gpu' in config:
+        global gpu_id = config['gpu']
 
     params = RNNParams(sequence_length = config['rnn']['seq_len'],
                         input_size = config['rnn']['input'],
@@ -142,12 +147,19 @@ if __name__ == '__main__':
                         num_epochs = config['rnn']['epochs'],
                         learning_rate = config['rnn']['lr'])
 
-    print '>>> Loading datasets <<<'
-    train_dataset = load_ucf_dataset(config['train_path'])
-    test_dataset = load_ucf_dataset(config['test_path'])
+    if config['mode'] == 'train':
+        print '>>> Loading datasets <<<'
+        train_dataset = load_ucf_dataset(config['train_path'])
+        test_dataset = load_ucf_dataset(config['test_path'])
 
-    print '>>> Training the model <<<'
-    rnn_model = train(train_dataset,  params, config['models_dir'])
+        print '>>> Training the model <<<'
+        rnn_model = train(train_dataset,  params, config['models_dir'])
 
-    print '>>> Evaluating the model <<<'
-    eval(rnn_model, test_dataset, params)
+        print '>>> Evaluating the model <<<'
+        eval(rnn_model, test_dataset, params)
+
+    elif config['mode'] == 'test':
+        test_dataset = load_ucf_dataset(config['test_path'])
+
+        print '>>> Testing the model <<<'
+        test(config['model'], test_dataset, params)
