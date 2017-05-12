d7e6e0278f21ab493a48d06431e2561ac75a92f3
salmedina <salbed.medina@gmail.com>
2017-05-12 19:37:56 -0400

    debug data_proc

--- src/data_proc.py
+++ src/data_proc.py
@@ -238,7 +238,7 @@ def extract_data_charades(data_path, annotation_list, num_frames, actions_dict):
                 video_poses = np.concatenate((video_poses, [last_pose]))
 
         if missing_pose_count > 0:
-            print '[{}] {} Missing pose frames: {}/{}'.format(processing_pos, video_id, missing_pose_count, len(video_poses))
+            print '[{}] {} {} Missing pose frames: {}/{}'.format(processing_pos, video_id, clip_label, missing_pose_count, len(video_poses))
 
         if len(video_poses) < REQ_FRAMES:
             # Loop through the video until we get the required length
