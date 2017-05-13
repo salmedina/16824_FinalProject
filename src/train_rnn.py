305d192cbf87644362c1baf2f23fa4c00de77974
salmedina <salbed.medina@gmail.com>
2017-05-13 16:08:20 -0400

    add using gpu msg

--- src/train_rnn.py
+++ src/train_rnn.py
@@ -146,6 +146,7 @@ if __name__ == '__main__':
 
     if use_cuda and 'gpu' in config:
         set_gpu_id(config['gpu'])
+        print 'Using GPU: {}'.format(gpu_id)
 
     params = RNNParams(sequence_length = config['rnn']['seq_len'],
                         input_size = config['rnn']['input'],
